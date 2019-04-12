555275bcd44540a21c4e02be2377b9951e3792c6
jbloom <jbloom@fredhutch.org>
2019-04-12 15:20:30 -0700

    compare neut and binding titers

---
+++ run_notebook.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+RESULTSDIR="results/notebooks"
+
+mkdir -p $RESULTSDIR
+
+declare -a nbs=("analyze_neut.ipynb")
+
+for nb in "${nbs[@]}"
+do
+    echo "Running $nb"
+
+    jupyter nbconvert \
+        --to notebook \
+        --execute \
+        --inplace \
+        --ExecutePreprocessor.timeout=-1 \
+        $nb
+
+    echo "Converting $nb to Markdown"
+    jupyter nbconvert \
+        --output-dir $RESULTSDIR \
+        --to markdown \
+        $nb
+done
